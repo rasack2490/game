4feca9bcfb690fac87ca7f477d590883ca86ee8f
programmer <programmer@localhost.localdomain>
2020-08-20 11:05:48 +0000

    creation de la page inscription

---
+++ inscription.php
@@ -0,0 +1,70 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+
+    
+  </head>
+  <body>
+    <div class="container">
+        <div align="center">
+            <form method="post" action="">
+                <h1>INSCRIPTION</h1> <br> <br>
+                <table>
+                    <tr>
+                        <td align="right">
+                            <label for="speudo" >Speudo :</label>
+                        </td>
+                        <td>
+                            <input type="text" placeholder="votre speudo" name="speudo"/>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td align="right">
+                            <label for="mail" >Mail :</label>
+                        </td>
+                        <td>
+                            <input type="email" placeholder="votre mail" name="mail"/>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td align="right">
+                            <label for="mail2" >Confirmation mail :</label>
+                        </td>
+                        <td>
+                            <input type="email" placeholder="votre mail de confirmation" name="mail2"/>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td align="right">
+                            <label for="mdp" >Mot de passe :</label>
+                        </td>
+                        <td>
+                            <input type="password" placeholder="votre mot de passe" name="mdp"/>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td align="right">
+                            <label for="mdp2" >Confirmation du mot de passe :</label>
+                        </td>
+                        <td>
+                            <input type="password" placeholder="votre mot de passe de confirmation" name="mdp2"/>
+                        </td>
+                    </tr>
+                </table> <br>
+                <input type="submit" value="je m'inscris">
+            </form>
+        </div>
+    </div>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+  </body>
+</html>
